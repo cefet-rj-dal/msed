98cd156af379c06725b9cb89ae25fc7b09884bbc
Diego Salles <diego.salles@cortex-intelligence.com>
2023-07-23 19:22:17 -0300

    test

--- componentes.R
+++ componentes.R
@@ -11,6 +11,7 @@ library(magrittr)
 source("moghtaderi-filter.R")
 
 
+
 ceemd_trend_filter <- function(serie=NULL, noise.amp = 6.4e-07 , trials = 100, limit_ri_sup = 2.14, limit_ri_inf = 1.50, frequency_date=12, start_date=c(2004,1)
                                , stop_rule="type3"){
 
